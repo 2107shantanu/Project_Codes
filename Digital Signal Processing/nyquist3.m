53648e4ff95be8da44b495461faa11b9abe2f4d6
2107shantanu <54627896+2107shantanu@users.noreply.github.com>
2021-11-20 14:49:27 +0530

    Create nyquist3.m

---
+++ Digital Signal Processing/nyquist3.m
@@ -0,0 +1,25 @@
+amp=1;                    %amp=amplitude
+time=2;
+f1=20;                    %f1=Frequency of 1st component signal           
+f2=30;                    %f2=Frequency of 2nd component signal
+fs=30;                    %fs=Sampling Frequency
+T=1/fs;
+t=0:T:time;
+x=amp*sin(2*pi*f1*t)+amp*sin(2*pi*f2*t);    %Signal
+figure(3)
+subplot(2,1,1);
+plot(t,x);
+title('Wave Signal 30 Hz');
+xlabel('Time');
+ylabel('Amplitude');
+
+N=length(x);
+fftSignal=fft(x);
+fftSignal=fftshift(fftSignal);
+f=fs*(-N/2:N/2-1)/N;
+figure(3)
+subplot(2,1,2);
+plot(f,abs(fftSignal));
+title('FFT wave signal 30 Hz');           %FFT Signal
+xlabel('Frequency');
+ylabel('Magnitude');
